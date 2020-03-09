f8e1a6b8133878abab428abff355dcd787defc1c
Eric Texier <etexier@student.42.fr>
2020-03-09 10:50:54 +0100

    update install script

--- install_shell/bin/do_pre_install_envi.sh
+++ install_shell/bin/do_pre_install_envi.sh
@@ -6,7 +6,10 @@ mkdir -p dev/dsk_configuration/envi/configs_and_packs/configs
 mkdir -p packages
 dir=`pwd`
 echo "add_pack('dskenvi')" > dev/dsk_configuration/envi/configs_and_packs/configs/init.py
-echo "import os" > dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "base_release = '"${dir}"/packages/dsk_envi'" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "version = ''" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
+
+
+TargetEnvi=dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
+echo "import os" > ${TargetEnvi}
+echo "base_release = '"${dir}"/packages/dskenvi'" >> ${TargetEnvi}
+echo "version = ''" >> ${TargetEnvi}
+echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> ${TargetEnvi}
--- install_shell/bin/do_pre_install_envi_dev.sh
+++ install_shell/bin/do_pre_install_envi_dev.sh
@@ -6,15 +6,18 @@ mkdir -p dev/dsk_configuration/envi/configs_and_packs/configs
 mkdir -p packages
 dir=`pwd`
 echo "add_pack('dskenvi')" > dev/dsk_configuration/envi/configs_and_packs/configs/init.py
-echo "add_pack('devsoftkit')" >> dev/dsk_configuration/envi/configs_and_packs/configs/init.py
+echo "add_pack('dsksoftkit')" >> dev/dsk_configuration/envi/configs_and_packs/configs/init.py
 
-echo "import os" > dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "base_release = '"${dir}"/packages/dsk_envi'" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "version = ''" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
-echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
 
-echo "import os" > dev/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
-echo "base_release = '"${dir}"/packages/devsoftkit'" >> dev/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
-echo "version = ''" >> dev/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
-echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> dev/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
-echo "add_path('PATH', os.path.join(base_release, version, 'bin'))" >> dev/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
+TargetEnvi=dev/dsk_configuration/envi/configs_and_packs/packs/dskenvi.py
+echo "import os" > ${TargetEnvi}
+echo "base_release = '"${dir}"/packages/dskenvi'" >> ${TargetEnvi}
+echo "version = ''" >> ${TargetEnvi}
+echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> ${TargetEnvi}
+
+TargetSoft=dev/dsk_configuration/envi/configs_and_packs/packs/dsksoftkit.py
+echo "import os" > ${TargetSoft}
+echo "base_release = '"${dir}"/packages/dsksoftkit'" >> ${TargetSoft}
+echo "version = ''" >> ${TargetSoft}
+echo "add_path('PYTHONPATH', os.path.join(base_release, version, 'python'))" >> ${TargetSoft}
+echo "add_path('PATH', os.path.join(base_release, version, 'bin'))" >> ${TargetSoft}
