213449a16cb6252cdccfa93039f4d2307ec299d1
Eric Texier <etexier@student.42.fr>
2020-03-09 14:27:53 +0100

    remove devsoftkit

--- install_shell/dsk_configuration/envi/configs_and_packs/packs/devsoftkit.py
+++
@@ -1,8 +0,0 @@
-import os
-base_release = "/Users/eric/mnt/dev/eric/packages/devsoftkit"
-version = ""
-
-
-add_path("PYTHONPATH",os.path.join(base_release, version, "python"))
-add_path("PATH",os.path.join(base_release, version, "bin"))
-
